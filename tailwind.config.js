f2899eae53f46f3c4baa97fcd238178cc69b854e
prabh1234assla <pdsinghassla@gmail.com>
2023-12-26 16:16:52 +0530

    scrub just added via gsap

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,45 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'one': {
+          100: '#FF0465',
+          200: '#FF0101'
+        },
+        'two': {
+          100: '#344468',
+          200: '#FFC700'
+        },
+        'three': {
+          100: '#293504',
+          200: '#FF4B30'
+        },
+        'four': {
+          100: '#FF7F00',
+          200: '#FF7F00'
+        },
+        'five': {
+          100: '#DA121F',
+          200: '#FF0030'
+        },
+        'six': {
+          100: '#00614C',
+          200: '#FE007A'
+        },
+        'seven': {
+          100: '#B20160',
+          200: '#FF0000'
+        },
+        'eight': {
+          100: '#004A53',
+          200: '#00E0FF'
+        }
+      },
+      fontFamily: {
+        meander: "Meander"
+      }
+    },
   },
   plugins: [],
 }
